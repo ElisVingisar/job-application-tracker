484f3b23f68f4c37dd07bb63a3a4976315c8087e
elis <elis.vingisar@gmail.com>
2026-02-19 14:38:25 +0200

    feat(repository): add application email-based queries

--- backend/src/main/java/com/jobtracker/jobtracker/repository/ApplicationRepository.java
+++ backend/src/main/java/com/jobtracker/jobtracker/repository/ApplicationRepository.java
@@ -2,9 +2,10 @@ package com.jobtracker.jobtracker.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.jobtracker.jobtracker.model.Application;
-import com.jobtracker.jobtracker.model.User;
 import java.util.List;
+import java.util.Optional;
 
 public interface ApplicationRepository extends JpaRepository<Application, Long>{
-    List<Application> findByUser(User user);
+    List<Application> findByUserEmail(String email);
+    Optional<Application> findByIdAndUserEmail(Long id, String email);
 }
